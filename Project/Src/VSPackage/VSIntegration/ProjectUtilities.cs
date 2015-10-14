df7dcf5cbded9edd4b290c759fbec66650530c33
ptittof57 <ptit_tof57@hotmail.fr>
2015-10-14 21:56:12 +0200

    Fix issue with xproj

--- Project/Src/VSPackage/VSIntegration/ProjectUtilities.cs
+++ Project/Src/VSPackage/VSIntegration/ProjectUtilities.cs
@@ -1384,6 +1384,13 @@ namespace StyleCop.VisualStudio
 
             try
             {
+                Proj
+                // Analyze cs file for ASP V5
+                if (item.Kind == Constants.vsProjectItemKindPhysicalFile && item.ContainingProject.Kind == "{8BB2217D-0F2D-49D1-97BC-3654ED321F3B}")
+                {
+                    return true;
+                }
+                 
                 Property buildAction = item.Properties.Item("BuildAction");
                 if (buildAction == null)
                 {
