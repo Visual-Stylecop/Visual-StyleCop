59e043e370bcb4bd379b47a0351e26d7fd5af89a
Christophe HEISER <ptit_tof57@hotmail.fr>
2016-02-27 12:04:54 +0100

    Add command from init script in one script

--- Build/Install.ps1
+++ Build/Install.ps1
@@ -1,2 +1,7 @@
+git config --global core.autocrlf true
+
+# Remote Desktop information
+iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/appveyor/ci/master/scripts/enable-rdp.ps1'))
+
 # Import extension to update Vsix version
 Import-Module ./Build/Extensions/vsix.ps1 | iex
